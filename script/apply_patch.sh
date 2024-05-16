ef0496df577b6c34505a8bb5b39f98f8cab8269f
Shoei Nashimoto <Nashimoto.Shoei@bx.MitsubishiElectric.co.jp>
2024-05-16 14:51:06 +0900

    bugfix: fix patching directory and variable declaration (extern)

--- script/apply_patch.sh
+++ script/apply_patch.sh
@@ -3,12 +3,13 @@ KEYSTONE_DIR="$PWD/../keystone"
 LOCAL_SM_DIR="$PWD/../sm"
 LOCAL_KSDRV_DIR="$PWD/../linux-keystone-driver"
 KS_SM_DIR="$KEYSTONE_DIR/sm"
-KS_SKDRV_DIR="$KEYSTONE_DIR/linux-keystone-driver"
+KS_KSDRV_DIR="$KEYSTONE_DIR/linux-keystone-driver"
 
 # copy files
-cp -f "$LOCAL_SM_DIR/conf.h" "$KS_SM_DIR/"
-cp -f "$LOCAL_SM_DIR/jam.*" "$KS_SM_DIR/"
-cp -f "$LOCAL_KSDRV_DIR/conf.h" "$KS_KSDRV_DIR/"
+cp "$LOCAL_SM_DIR/conf.h" "$KS_SM_DIR/src/"
+cp "$LOCAL_SM_DIR/jam.c" "$KS_SM_DIR/src/"
+cp "$LOCAL_SM_DIR/jam.h" "$KS_SM_DIR/src/"
+cp "$LOCAL_KSDRV_DIR/conf.h" "$KS_KSDRV_DIR/"
 
 # apply patches
 cd $KS_SM_DIR
